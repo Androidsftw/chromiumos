28c665039cd946d9fe3605a70cd01ad7a7a6610e
sixsixfive <punx665@gmail>
2014-04-13 21:46:23 +0200

    initial

---
+++ README.md
@@ -0,0 +1,32 @@
+This is just a poor script for [ArnoldTheBats](http://arnoldthebat.co.uk/wordpress/chromium-os/) ChromiumOS builds to get flash and Co.
+
+## Install
+Just copy the installcodecs.sh & pepper-flash.info into your Downloads folder:
+
+* open crosh: **Press CTRL+ALT+T**
+* Enter: **`shell`**
+* enter the Download directrory: **`cd ~/Downloads`**
+* become root: **`sudo su`**
+* run the script: **`bash installcodecs.sh`**
+* if everything went well: **`reboot`**
+   
+#### Default ChromeOS Applications
+* [Calculator](https://chrome.google.com/webstore/detail/calculator/joodangkbfjnajiiifokapkpmhfnpleo)
+* [Google Keep](https://chrome.google.com/webstore/detail/google-keep/hmjkmjkepdijhoojdojkdfohbdgmmhki)
+* [Remote Desktop](https://chrome.google.com/webstore/detail/chrome-remote-desktop/gbchcmhmhahfdphkhkmpfmihenigjmpp)
+* [Camera](https://chrome.google.com/webstore/detail/camera/hfhhnacclhffhdffklopdkcgdhifgngh)
+* [G+](https://chrome.google.com/webstore/detail/google%20/dlppkpafhbajpcmmoheippocdidnckmm)
+* [Google+ Photos](https://chrome.google.com/webstore/detail/google%20-photos/efjnaogkjbogokcnohkmnjdojkikgobo)
+* [Google Drive](https://chrome.google.com/webstore/detail/google-drive/apdfllckaahabafndbhieahigkjlhalf)
+* [Google Calendar](https://chrome.google.com/webstore/detail/google-calendar/ejjicmeblgpmajnghnpcppodonldlgfn)
+* [Google Maps](https://chrome.google.com/webstore/detail/google-maps/lneaknkopdijkpnocmklfnjbeapigfbh)
+* [Google Mail](https://chrome.google.com/webstore/detail/gmail/pjkljhegncpnkpknbcohdijeoejaedia)
+* [Google Search](https://chrome.google.com/webstore/detail/google-search/coobgpohoikkiipiblmjeljniedjpjpf)
+* [Google Docs](https://chrome.google.com/webstore/detail/google-docs/aohghmighlieiainnegkcijnfilokake)
+* [Google Sheets](https://chrome.google.com/webstore/detail/google-sheets/felcaaldnbdncclmgdcncolpebgiejap)
+* [Google Slides](https://chrome.google.com/webstore/detail/google-slides/aapocclcgogkmnckokdopfmhonfmgoek)
+* [Google Store Link](https://chrome.google.com/webstore/detail/games/fobcpibfeplaikcclojfdhfdmbbeofai)
+* [YouTube](https://chrome.google.com/webstore/detail/youtube/blpcfgokakmgnkcojhhkbfbldkacnbeo)
+* [Google Play Music](https://chrome.google.com/webstore/detail/google-play-music/icppfcnhkcmnfdhfhphakoifcfokfdhg)
+* [Google Play Books](https://chrome.google.com/webstore/detail/google-play-books/mmimngoggfoobjdlefbcabngfnmieonb)
+* [Google Play Movies](https://chrome.google.com/webstore/detail/google-play-movies/fppdphmgcddhjeddoeghpjefkdlccljb)
---
+++ installcodecs.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+#based on https://wiki.archlinux.org/index.php/Chromium
+#based on https://gist.github.com/dz0ny/3065781
+base="$(dirname "$(readlink -f "${0}")")"
+chromebrewbinutils="https://raw.githubusercontent.com/skycocker/chromebrew/master/packages/binutils.rb"
+
+#codecs are only available for x86 cpus
+if [ $(uname -m) != "i686" ] && [ $(uname -m) != "x86_64" ]; then
+  echo 'Only Intel x86 compatible CPUs are supported'
+  exit 1;
+fi
+
+#chrome links
+if [ `uname -m` == 'x86_64' ]; then
+  # 64-bit
+  export CHROME="https://dl.google.com/linux/direct/google-chrome-unstable_current_amd64.deb"
+else
+  # 32-bit
+  export CHROME="https://dl-ssl.google.com/linux/direct/google-chrome-unstable_current_i386.deb"
+fi
+
+##checkforoldcrap
+cd "$base"
+if [ -f "$base"/chrome-bin.deb ]; then
+	echo "deleting crap"
+	rm "$base"/chrome-bin.deb
+else
+	echo "..."
+fi
+
+if [ -f "$base"/chrome.tar.lzma ]; then
+	echo "deleting crap"
+	rm "$base"/chrome.tar.lzma
+else
+	echo "..."
+fi
+
+if [ -d "$base"/chrome-unstable ]; then
+	echo "deleting crap"
+	rm -rf "$base"/chrome-unstable
+else
+	echo "..."
+fi
+
+#remount the root partition
+echo "remount rootfs" && sleep 5
+mount -o remount, rw /
+
+#gettingbinutils
+echo "downloading binutils" && sleep 5
+wget "$chromebrewbinutils" -O "$base"/binutils.rb
+binutilsurl=`cat "$base"/binutils.rb | grep "https://" | grep "$(uname -m)" | tr "'" '"' | sed -n '/"/!{/\n/{P;b}};s/"/\n/g;D'`
+wget --progress=dot $binutilsurl -O "$base"/binutils.tgz
+tar -zxvf "$base"/binutils.tgz usr/local/i686-pc-linux-gnu/bin/ar
+cp "$base"/usr/local/i686-pc-linux-gnu/bin/ar /usr/bin
+
+if [ -f /usr/bin/ar ]; then
+	echo "ar found"
+	rm -rf "$base"/usr
+	rm -f "$base"/binutils.*
+else
+	echo "couldn't find ar - something went wrong - aborting!"
+	exit 1;
+fi
+
+#remove that ugly string at the login screen
+sed -i '/CHROMEOS_RELEASE_DESCRIPTION/d' /etc/lsb-release
+
+#codecs and other cool stuff
+echo "Downloading codecs" && sleep 5
+wget --progress=dot $CHROME -O "$base"/chrome-bin.deb
+mkdir "$base"/chrome-unstable
+/usr/bin/ar -p "$base"/chrome-bin.deb data.tar.lzma >> "$base"/data.tar.lzma
+tar -xvf "$base"/data.tar.lzma -C "$base"/chrome-unstable
+
+if [ -f "$base"/data.tar.lzma ]; then
+	echo "success" && sleep 5
+    rm "$base"/chrome-bin.deb
+    rm "$base"/data.tar.lzma
+else
+	echo "something went wrong - aborting!"
+	exit 1;
+fi
+
+echo "Installing codecs" && sleep 5
+#codecs
+cp "$base"/chrome-unstable/opt/google/chrome-unstable/libffmpegsumo.so "/opt/google/chrome" -f
+cp "$base"/chrome-unstable/opt/google/chrome-unstable/libppGoogleNaClPluginChrome.so "/opt/google/chrome" -f
+cp "$base"/chrome-unstable/opt/google/chrome-unstable/libwidevinecdm.so "/opt/google/chrome" -f
+cp "$base"/chrome-unstable/opt/google/chrome-unstable/libwidevinecdmadapter.so "/opt/google/chrome" -f
+cp "$base"/chrome-unstable/opt/google/chrome-unstable/libpdf.so "/opt/google/chrome" -f
+
+#peerconnect
+cp -R "$base"/chrome-unstable/opt/google/chrome-unstable/lib /opt/google/chrome
+
+#flash
+mkdir -p /opt/google/chrome/pepper
+cp "$base"/chrome-unstable/opt/google/chrome-unstable/PepperFlash/libpepflashplayer.so /opt/google/chrome/pepper/ -f
+cp "$base"/chrome-unstable/opt/google/chrome-unstable/PepperFlash/manifest.json /opt/google/chrome/pepper/ -f
+cp "$base"/pepper-flash.info /opt/google/chrome/pepper/ -f
+
+#remove chrome-dir stuff
+rm -rf "$base"/chrome-unstable
+
+echo "done cross fingers and reboot"
+sleep 5
+echo "PS: type reboot to reboot :P"
