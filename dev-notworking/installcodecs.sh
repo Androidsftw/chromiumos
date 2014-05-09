ce0258361b614b2cd5ebed99cafc67dc07a219c7
sixsixfive <punx665@gmail.com>
2014-05-09 22:32:32 +0200

    commit

--- dev-notworking/installcodecs.sh
+++ dev-notworking/installcodecs.sh
@@ -4,8 +4,8 @@ base="$(dirname "$(readlink -f "${0}")")"
 #codecs are only available for x86 cpus
 if [ `uname -m` == 'x86_64' ]; then
 	chromeurl="https://dl.google.com/linux/direct/google-chrome-unstable_current_amd64.deb"
-	gtalkurl="https://drive.google.com/file/d/0B_2_dsXrefR-RUcxbXhhUEdSdXc/edit?usp=sharing"
-	netflixurl="https://drive.google.com/file/d/0B_2_dsXrefR-UWV3czFqTWpmSEE/edit?usp=sharing"
+	gtalkurl="https://drive.google.com/file/d/0B_2_dsXrefR-UDlXSUpIdTZUUE0/edit?usp=sharing"
+	netflixurl="https://drive.google.com/file/d/0B_2_dsXrefR-S2cxYVRTdEpfQUE/edit?usp=sharing"
 	arurl="https://github.com/sixsixfive/chromiumos/raw/master/dev-notworking/ar-binutils-2.23.2-chromebrew/amd64/ar"
 elif [ $(uname -m) != "i686" ]
 	cromeurl="https://dl-ssl.google.com/linux/direct/google-chrome-unstable_current_i386.deb"
@@ -25,74 +25,87 @@ mount -o remount, rw /
 #remove that ugly string at the login screen
 sed -i '/CHROMEOS_RELEASE_DESCRIPTION/d' /etc/lsb-release
 
-#download ar
-echo "downloading GNU ar"
-sleep 3
-wget --progress=dot "$arurl" -O /usr/bin/ar 2>&1 | grep --line-buffered "%"
-clear
-if [ -f /usr/bin/ar ]; then
-	echo "ar found"
-	sleep 3
-else
-	echo "couldn't find ar - something went wrong - aborting!"
-	mount -o remount, r /
-	exit 1;
-fi
-
 #creating the pepper dir and a tmpdir
 mkdir -p /opt/google/chrome/pepper
 mkdir -p "$base"/.codectmp
 
+#download ar
+if [ -f "$base"/.codectmp/ar ]; then
+	echo "GNU ar found"
+	sleep 3
+else
+	echo "downloading GNU ar"
+	sleep 3
+	wget --progress=dot "$arurl" -O "$base"/.codectmp/ar 2>&1 | grep --line-buffered "%"
+fi
+
+if [ -f "$base"/.codectmp/ar ]; then
+	echo "installing ar"
+	sleep 3
+	cp "$base"/.codectmp/ar /usr/bin/ar -f
+	chmod +x /usr/bin/ar
+	if [ -f /usr/bin/ar ]; then
+		echo "ar installed"
+	else
+		echo "couldn't find ar - something went wrong - aborting!"
+		mount -o remount, r /
+		exit 1;
+	fi
+else
+	echo "couldn't download ar - aborting! (-_-)"
+	mount -o remount, r /
+	exit 1;
+fi
+
 #download peppertalk
-if [ -f "$base"/.codectmp/google-talk-pepper.txz ]; then
+if [ -f "$base"/.codectmp/google-talk-pepper.tbz ]; then
 	echo "gtalk found"
 	sleep 3
 else
 	echo "downloading peppertalk"
 	sleep 3
-	wget --progress=dot "$gtalkurl" -O "$base"/.codectmp/google-talk-pepper.txz 2>&1 | grep --line-buffered "%"
-	clear
+	wget --progress=dot "$gtalkurl" -O "$base"/.codectmp/google-talk-pepper.tbz 2>&1 | grep --line-buffered "%"
 fi
 
-if [ -f "$base"/.codectmp/google-talk-pepper.txz ]; then
+if [ -f "$base"/.codectmp/google-talk-pepper.tbz ]; then
 	echo "extracting peppertalk"
 	sleep 3
 	cd /
-	tar xvJf "$base"/.codectmp/google-talk-pepper.txz
+	tar xfvj "$base"/.codectmp/google-talk-pepper.tbz
 	if [-f /opt/google/talkplugin/GoogleTalkPlugin]; then
 		echo "GTalkPlugin installed"
 	else
 		echo "FAIL (-_-)"
+	fi
 else
 	echo "couldn't download peppertalk (-_-)"
 fi
 
 #download netflix
-if [ -f "$base"/.codectmp/netflixhelper.txz ]; then
+if [ -f "$base"/.codectmp/netflixhelper.tbz ]; then
 	echo "netflix found"
 	sleep 3
 else
 	echo "downloading netflixplugin"
 	sleep 3
-	wget --progress=dot "$netflixurl" -O "$base"/.codectmp/netflixhelper.txz 2>&1 | grep --line-buffered "%"
-	clear
+	wget --progress=dot "$netflixurl" -O "$base"/.codectmp/netflixhelper.tbz 2>&1 | grep --line-buffered "%"
 fi
 
-if [ -f "$base"/.codectmp/netflixhelper.txz ]; then
+if [ -f "$base"/.codectmp/netflixhelper.tbz ]; then
 	echo "extracting netflixplugin"
 	sleep 3
 	cd /
-	tar xvJf "$base"/.codectmp/netflixhelper.txz
+	tar xfvj "$base"/.codectmp/netflixhelper.tbz
 	if [-f /opt/google/chrome/pepper/libnetflixhelper.so]; then
 		echo "Netflix Plugin installed"
 	else
 		echo "FAIL (-_-)"
+	fi
 else
 	echo "couldn't download netflixplugin (-_-)"
 fi
 
 #adobe stuff
-clear
 echo "Downloading the adobe pepper plugins"
 sleep 3
 wget --progress=dot "$chromeurl" -O "$base"/.codectmp/chrome-bin.deb 2>&1 | grep --line-buffered "%"
@@ -132,6 +145,28 @@ echo -e "FILE_NAME=/opt/google/chrome/pepper/libpepflashplayer.so\nPLUGIN_NAME=\
 rm -rf "$base"/.codectmp/chrome-unstable
 #remount the rootfs
 mount -o remount, r /
-echo "done, cross fingers and reboot"
+
+#installation status
+if [-f /opt/google/talkplugin/GoogleTalkPlugin]; then
+		echo "GTalkPlugin		OK"
+else
+		echo "GTalkPlugin		FAILED"
+fi
+if [-f /opt/google/chrome/pepper/libnetflixhelper.so]; then
+		echo "NetflixPlugin		OK"
+else
+		echo "NetflixPlugin		FAILED"
+fi
+if [-f /opt/google/chrome/pepper/libpepflashplayer.so]; then
+		echo "FlashPlugin		OK"
+else
+		echo "FlashPlugin		FAILED"
+fi
+if [-f /opt/google/chrome-unstable/libpdf.so]; then
+		echo "PDFPlugin			OK"
+else
+		echo "PDFPlugin			FAILED"
+fi
+echo "done, rebooting in 5 seconds"
 sleep 5
-echo "PS: type reboot to reboot :P"
+#reboot
